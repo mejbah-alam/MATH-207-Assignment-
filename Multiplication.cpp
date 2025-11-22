3dd7bfc9ac73d8c8858786b126ab6c619cf9b365
Mejbahul Alam <mejbah96alam@gmail.com>
2025-11-22 19:23:19 +0600

    Add files via upload

---
+++ Multiplication.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m1, n1, m2, n2, k, row, col, sum = 0;
+    cout << "Enter Rows Number for 1st Matrix: ";
+    cin >> m1;
+    cout << "Enter Columns Number for 1st Matrix: ";
+    cin >> n1;
+    cout << "Enter Rows Number for 2nd Matrix: ";
+    cin >> m2;
+    cout << "Enter Columns Number for 2nd Matrix: ";
+    cin >> n2;
+    int mat1[m1][n1], mat2[m2][n2], result[m1][n2];
+    if ((n1 != m2))
+    {
+        cout << endl << "Matrix Multiplication Not Possible!!!";
+        return 0;
+    }
+    cout << endl << "Enter Elements for 1st Matrix: " << endl;
+    for (row = 0; row < m1; row++)
+    {
+        for (col = 0; col < n1; col++)
+        {
+            cout << "mat1[" << row << "][" << col << "]: ";
+            cin >> mat1[row][col];
+        }
+        cout << endl;
+    }
+    cout << "Enter Elements for 2nd Matrix: " << endl;
+    for (row = 0; row < m2; row++)
+    {
+        for (col = 0; col < n2; col++)
+        {
+            cout << "mat2[" << row << "][" << col << "]: ";
+            cin >> mat2[row][col];
+        }
+        cout << endl;
+    }
+    
+    for (row = 0; row < m1; row++)
+    {
+        for (col = 0; col < n2; col++)
+        {
+            for (k = 0; k < n1; k++)
+            {
+                sum = sum + (mat1[row][k] * mat2[k][col]);
+            }
+            result[row][col] = sum;
+            sum = 0;
+        }
+    }
+    
+    cout << endl << "Multiplication of 1st & 2nd Matrix:" << endl;
+    for (row = 0; row < m1; row++)
+    {
+        for (col = 0; col < n2; col++)
+        {
+            cout << result[row][col] << "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
